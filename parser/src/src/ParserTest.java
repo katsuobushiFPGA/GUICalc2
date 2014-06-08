c1a7f74b53796d2db6b1ff8b91f9bee0712e455c
katsuobushi <katsuobushiFPGA>
2014-06-08 14:12:59 +0900

    a

---
+++ parser/src/src/ParserTest.java
@@ -0,0 +1,11 @@
+import java.text.ParseException;
+
+
+class ParserTest {
+	public static boolean flag=false;
+    public static void main(String[] arg) throws ParseException {
+		//title,width,height
+		CalcView v = new CalcView("電卓",600,700);
+		v.setLocationRelativeTo(null);
+    }
+}
